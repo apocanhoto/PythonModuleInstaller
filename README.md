7ed3525676c9a6ff43063207ed9b672d58acfb5d
Parrot Live user <user@localhost.localdomain>
2019-07-02 16:53:44 +0000

    Jun 2

---
+++ README.md
@@ -0,0 +1,10 @@
+# Description
+This script searches a term from the list https://pypi.org/simple/ and installs Python modules.
+
+
+# To Do
+- 
+- 
+- 
+
+
--- main.bash
+++ main.bash
@@ -2,21 +2,38 @@
 
 wget -O tempModuleDB.html https://pypi.org/simple/
 
-read "What term would you like to search for?" search
-for i in $(cat index.html | grep $search | awk -F"/" '{print $3}'); do
-	echo "do you wish to install $i ? (yes/no/cancel)" 
-	read answer
+if [[ "$1"=="auto" ]]; then
+	read -p "What term would you like to search for? " search
+	for i in $(cat tempModuleDB.html | grep $search | awk -F"/" '{print $3}'); do
+		echo "##########################################################################"
+		echo "# Installing $i                                                          #"
+		echo "##########################################################################"
+		sudo pip3 install $i
+	done
+	echo "##########################################################################"
+	echo "# Done!                                                                  #"
+	echo "##########################################################################"
+
+
+	exit
+fi
+
+
+read -p "What term would you like to search for? " search
+for i in $(cat tempModuleDB.html | grep $search | awk -F"/" '{print $3}'); do
+	read -p "do you wish to install $i? (Yes/No/Cancel) " answer
 	case ${answer:0:1} in
-    [yY]* )
-        sudo pip3 install $i
-    ;;
-    [cC]* )
+	[yY]* )
+		echo "Installing $i"
+		sudo pip3 install $i
+	;;
+	[cC]* )
 		echo "Exiting..."
 		exit
 	;;
-    * )
-        echo "Module $i skipped."
-    ;;
+	* )
+		echo "Module $i skipped."
+	;;
 	esac
 	
 done
